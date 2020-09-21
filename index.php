88f2f465a4e572201a66cb9b84266b701ce0fe87
Eason900323 <C108118132@nkust.edu.tw>
2020-09-21 16:23:49 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<style>
+    body {
+        font-family:微軟正黑體;
+    }
+</style>
+<title>王乙澂0921</title>
+</head>
+<body>
+<h2>王乙澂0921 GIT練習紀錄</h2>
+<hr>
+<?php include "menu.php"; ?>
+<hr>
+<?php include "footer.php"; ?>
+</body>
+</html>
